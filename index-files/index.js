f916e19975cd97804089c6be8fcbb1fd08522cc6
qazser571 <107338889+qazser571@users.noreply.github.com>
2025-07-10 11:56:58 +0900

    Update index.js

--- index-files/index.js
+++ index-files/index.js
@@ -1,1 +1,6 @@
-
+function goToMainPage() {
+            window.location.href = "./codon/codon.html";
+        }
+        function goToTestPage() {
+            window.location.href = "./test/test.html";
+        }
