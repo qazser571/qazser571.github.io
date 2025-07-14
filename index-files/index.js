472b9e50a8c8bccdbeceb8108e48bfc1b66fc832
qazser571 <107338889+qazser571@users.noreply.github.com>
2025-07-14 16:31:47 +0900

    Delete index-files/index.js

--- index-files/index.js
+++
@@ -1,9 +0,0 @@
-function goToTimerPage() {
-    window.location.href = "./schedule/main.html";
-}
-function goToCodonPage() {
-    window.location.href = "./codon/main.html";
-}
-function goToTestPage() {
-    window.location.href = "./tempt/test.html";
-}
