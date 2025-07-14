91bf0e4f11a531e32fd73f5ba577af2ee2a564e7
qazser571 <107338889+qazser571@users.noreply.github.com>
2025-07-14 16:32:10 +0900

    Create script.js

---
+++ index-files/script.js
@@ -0,0 +1,9 @@
+function goToTimerPage() {
+    window.location.href = "./schedule/main.html";
+}
+function goToCodonPage() {
+    window.location.href = "./codon/main.html";
+}
+function goToTestPage() {
+    window.location.href = "./tempt/test.html";
+}
