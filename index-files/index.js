e5fc651fdf776f99edc4fd81ae52808b0144bfd3
qazser571 <107338889+qazser571@users.noreply.github.com>
2025-07-14 13:36:15 +0900

    Update index.js

--- index-files/index.js
+++ index-files/index.js
@@ -1,5 +1,5 @@
 function goToTimerPage() {
-    window.location.href = "./record-timer/main.html";
+    window.location.href = "./record-timer/test22.html";
 }
 function goToCodonPage() {
     window.location.href = "./codon/main.html";
